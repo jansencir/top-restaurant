386069e50c0271d737a4b734e9c298a62a778a3d
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-11-10 09:23:04 -1000

    Add psuedocode to index.js
    Trying to map out how the page will work

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+ * we have a webpage that has 3 buttons, home, menu, and info
+ * we need to load an initial webpage with the homepage content
+ * it should load the webpage initially upon opening
+ * hold event listeners for each of the buttons
+ * when the buttons are clicked, it should clear the content BELOW the tabs (in the content id div?)
+ * then load the content associated with that tab in the content id div
+ */
+
+
+// pageLoad();
+
+// add event listener to buttons
+// each should first clear the tabs of the content div
