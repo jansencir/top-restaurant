0387e974cd0bc0a062b5266ef7684823639541e4
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-11-11 12:43:34 -1000

    Add home page html content

--- src/home.js
+++ src/home.js
@@ -1,26 +1,40 @@
-
 const homePage = () => {
     const content = document.getElementById("content");
-    const pageContent = document.createElement("div");
-    pageContent.classList.add("page-content");
 
     const header = document.createElement("h1");
     header.textContent = "Welcome to Hibiscus Cafe"
-    pageContent.appendChild(header);
+    content.appendChild(header);
     const hibiscus = document.createElement("img");
     hibiscus.src = "../src/assets/yellow-hibiscus.jpg";
-    pageContent.append(hibiscus);
+    content.append(hibiscus);
 
     const div1 = document.createElement("div");
     const paragraph1 = document.createElement("p");
-    paragraph1.textContent = "Experience local flavors with limitless servings"
+    paragraph1.textContent = "Experience local flavors";
     div1.appendChild(paragraph1)
     const image1 = document.createElement("img");
     image1.src = "../src/assets/hawaii-pool.jpg";
     div1.appendChild(image1);
 
-    content.appendChild(pageContent);
+    const div2 = document.createElement("div");
+    const image2 = document.createElement("img");
+    image2.src = "../src/assets/hawaii-dusk.jpg";
+    div2.appendChild(image2);
+    const paragraph2 = document.createElement("p");
+    paragraph2.textContent = "with limitless servings";
+    div2.appendChild(paragraph2);
+
+    const div3 = document.createElement("div");
+    const paragraph3 = document.createElement("p");
+    paragraph3.textContent = "in the Heart of Waikiki";
+    div3.appendChild(paragraph3);
+    const image3 = document.createElement("img");
+    image3.src = "../src/assets/waikiki.jpg";
+    div3.appendChild(image3);
+
     content.appendChild(div1);
+    content.appendChild(div2);
+    content.appendChild(div3);
 }
 
 export default homePage;
